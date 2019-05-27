c5d9b6d40c94f63f08531240012b29b861816a84
FatDong1 <xuhaodong66@foxmail.com>
2019-05-27 16:04:33 +0800

    test

--- src/pages/list/hot.vue
+++ src/pages/list/hot.vue
@@ -12,7 +12,7 @@ export default {
             
         }
     },
-                    mounted: function () {
+    mounted: function () {
                         
     },
     created: function () {
